d7e5a200d5967c2c6fe59c7c5faef84742af5b5a
Adam Suchi Hafizullah <adam.suchi@cladtek.com>
2024-05-12 02:15:47 +0700

    update: villa CRUD function

--- WhiteLagoon.Web/Controllers/HomeController.cs
+++ WhiteLagoon.Web/Controllers/HomeController.cs
@@ -23,9 +23,8 @@ public class HomeController : Controller
         return View();
     }
 
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        return View();
     }
 }
--- WhiteLagoon.Web/Controllers/VillaController.cs
+++ WhiteLagoon.Web/Controllers/VillaController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WhiteLagoon.Domain.Entities;
 using WhiteLagoon.Infrastructure.Data;
 
 namespace WhiteLagoon.Web.Controllers;
@@ -17,5 +18,83 @@ public class VillaController : Controller
         var villas = _db.Villas.ToList();
 
         return View(villas);
+    }
+
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult Create(Villa villa)
+    {
+        if (villa.Name == villa.Description)
+        {
+            ModelState.AddModelError("description", "The description cannot exactly match the name");    
+        }
+        
+        if (ModelState.IsValid)
+        {
+            _db.Villas.Add(villa);
+            _db.SaveChanges();
+            
+            return RedirectToAction("Index");
+        }
+
+        return View();
+    }
+
+    public IActionResult Update(int villaId)
+    {
+        Villa? villa = _db.Villas.FirstOrDefault(x => x.Id == villaId);
+        /*Villa? villa = _db.Villas.Find(villaId);
+        Villa? villa = _db.Villas.Where(x => x.Id == villaId);*/
+
+        if (villa is null)
+        {
+            return RedirectToAction("Error", "Home");
+        }
+
+        return View(villa);
+    }
+
+    [HttpPost]
+    public IActionResult Update(Villa villa)
+    {
+        if (ModelState.IsValid && villa.Id > 0)
+        {
+            _db.Villas.Update(villa);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        return View(villa);
+    }
+
+    public IActionResult Delete(int villaId)
+    {
+        Villa? villa = _db.Villas.FirstOrDefault(x => x.Id == villaId);
+
+        if (villa is null)
+        {
+            return RedirectToAction("Error", "Home");
+        }
+
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult Delete(Villa villa)
+    {
+        Villa? checkVilla = _db.Villas.FirstOrDefault(x => x.Id == villa.Id);
+
+        if (checkVilla is not null)
+        {
+            _db.Villas.Remove(checkVilla);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        return View();
     }
 }
